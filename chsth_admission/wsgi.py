c707951d4595dccbbdb98c83d03a4fccb202434f
Yusuf-Babagana <yusufmuhdkk@gmail.com>
2026-06-21 11:20:14 -0700

    A

--- chsth_admission/wsgi.py
+++ chsth_admission/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for chsth_admission project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# ===== PROJECT PATH (IMPORTANT) =====
+path = '/home/chfuntua/chfuntua'
+if path not in sys.path:
+    sys.path.append(path)
+
+# ===== DJANGO SETTINGS =====
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chsth_admission.settings'
+
+# ===== WSGI APPLICATION =====
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chsth_admission.settings')
-
 application = get_wsgi_application()
